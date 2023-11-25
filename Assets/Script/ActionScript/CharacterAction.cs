8832750527f0c37b9d385e68d05239b9027995d8
Daniel Lee <jhoon8903@gmail.com>
2023-11-25 17:14:34 +0900

    [💫 Feature] Character Animator
    플레이어 케릭터 움직임 Animator 적용

--- Assets/Script/ActionScript/CharacterAction.cs
+++ Assets/Script/ActionScript/CharacterAction.cs
@@ -1,12 +1,12 @@
 using System;
 using Script.Controller;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Script.ActionScript
 {
     public class CharacterAction  : MonoBehaviour
     {
-
         private MovementEventController _movementEventController;
         
         #region Move Variables
@@ -19,6 +19,13 @@ namespace Script.ActionScript
         [SerializeField] private SpriteRenderer character;
         #endregion
 
+        #region AnimationTrigger
+        [SerializeField] private Animator animator;
+        private enum Triggers { MOVE, IDLE }
+        private Triggers _trigger;
+
+        #endregion
+
         private void Awake()
         {
             _movementEventController = GetComponent<MovementEventController>();
@@ -29,6 +36,8 @@ namespace Script.ActionScript
         {
             _movementEventController.MoveEvent += Move;
             _movementEventController.LookForwardEvent += LookForward;
+            _trigger = Triggers.IDLE;
+            animator.SetTrigger(_trigger.ToString());
         }
 
         private void FixedUpdate()
@@ -49,8 +58,11 @@ namespace Script.ActionScript
 
         private void MoveToward(Vector2 moveDirection)
         {
+            _trigger = moveDirection != Vector2.zero ? Triggers.MOVE : Triggers.IDLE;
+            animator.SetTrigger(_trigger.ToString());
             Vector2 direction = moveDirection * speed;
             _rigidbody2D.velocity = direction;
+
         }
     }
 }
